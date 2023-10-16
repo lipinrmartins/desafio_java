4727e29ef9057ef4ca53b5fe480601920fe541b5
Felipe <felipe.martinsjiu@gmail.com>
2023-10-16 18:10:30 -0300

    Desafio em Java. Aprendendo nova tecnologia.

--- DesafioProposto.java
+++ DesafioProposto.java
@@ -7,6 +7,12 @@ public class DesafioProposto {
         double saldoConta = 1599.00;
         int escolhaOperacao = -1;
 
+        System.out.println("***********************");
+        System.out.println("\nNome do cliente: " + nome);
+        System.out.println("Tipo conta: " + tipoDeConta);
+        System.out.println("Saldo atual: " + saldoConta);
+        System.out.println("\n***********************");
+
         while (escolhaOperacao != 4){
             String menuOperacoes = String.format("""
                     Operações:
@@ -18,37 +24,37 @@ public class DesafioProposto {
                     Digite a opção desejada:""");
             System.out.println(menuOperacoes);
             int operacao = new Scanner(System.in).nextInt();
-
-            switch (operacao){
-                case 1:
-                    System.out.println("Saldo da conta R$ " + saldoConta);
-                    continue;
-
-                case 2:
-                    System.out.println("Informe o valor do Depósito:");
-                    double receberValor = new Scanner(System.in).nextDouble();
-                    saldoConta += receberValor;
-                    System.out.println("Você depositou a importância de R$ " + receberValor);
-                    System.out.println("Saldo Atulizado R$ " + saldoConta);
-                    continue;
-
-                case 3:
-                    System.out.println("Informe o valor da Transferência:");
-                    double transferirValor = new Scanner(System.in).nextDouble();
-                    if (transferirValor > saldoConta){
-                        System.out.println("Você não possui saldo suficiente para essa transação! Verifique seu saldo.");
-                    } else {
-                        System.out.println("Você transferiu a importância de R$ " + transferirValor);
-                        saldoConta -= transferirValor;
-                        System.out.println("Saldo Atualizado R$ " + saldoConta);
-                    }
-                    continue;
-                default:
-                    System.out.println("Opção inválida!");
-                    break;
+            if (operacao == 4){
+                System.out.println("Você encerrou as operações!");
+                escolhaOperacao = 4;
+            } else {
+                switch (operacao) {
+                    case 1:
+                        System.out.println("Saldo da conta R$ " + saldoConta);
+                        continue;
+                    case 2:
+                        System.out.println("Informe o valor do Depósito:");
+                        double receberValor = new Scanner(System.in).nextDouble();
+                        saldoConta += receberValor;
+                        System.out.println("Você depositou a importância de R$ " + receberValor);
+                        System.out.println("Saldo Atulizado R$ " + saldoConta);
+                        continue;
+                    case 3:
+                        System.out.println("Informe o valor da Transferência:");
+                        double transferirValor = new Scanner(System.in).nextDouble();
+                        if (transferirValor > saldoConta) {
+                            System.out.println("Você não possui saldo suficiente para essa transação! Verifique seu saldo.");
+                        } else {
+                            System.out.println("Você transferiu a importância de R$ " + transferirValor);
+                            saldoConta -= transferirValor;
+                            System.out.println("Saldo Atualizado R$ " + saldoConta);
+                        }
+                        continue;
+                    default:
+                        System.out.println("Opção inválida!");
+                        break;
+                }
             }
-            System.out.println("Operação Encerrada");
-            break;
         }
     }
 }
